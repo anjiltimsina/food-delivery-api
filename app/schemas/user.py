705b8fa038c558a0b29b164fe099d8062639af7b
Anjil Timsina <anjiltimsina1234@gmail.com>
2026-07-09 10:42:31 +0545

    Only one admin can exist before fully deploying

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,22 +3,29 @@ from app.models import UserRole
 from typing import Optional
 
 class UserRegister(BaseModel):
-    full_name : str
-    email:EmailStr
-    phone : str
-    password : str
-    role : UserRole = UserRole.CUSTOMER
+    full_name: str
+    email: EmailStr
+    phone: str
+    password: str
+    role: UserRole = UserRole.CUSTOMER
 
     @field_validator("password")
-    def password_strength(cls ,v):
-        if len(v) <8:
+    def password_strength(cls, v):
+        if len(v) < 8:
             raise ValueError("Password must be at least of 8 characters")
         return v
-    
+
     @field_validator("phone")
-    def phone_valid(cls ,v):
+    def phone_valid(cls, v):
         if not v.replace("+", "").isdigit():
             raise ValueError("Invalid Phone Number")
+        return v
+
+    @field_validator("role")
+    def restrict_public_roles(cls, v):
+        allowed_roles = {UserRole.CUSTOMER, UserRole.RESTAURANT_OWNER}
+        if v not in allowed_roles:
+            raise ValueError("Invalid role. Allowed roles are: customer, restaurant_owner")
         return v
     
 class UserLogin(BaseModel):
